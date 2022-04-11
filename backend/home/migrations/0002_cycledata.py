24bc24fb36d82e41cdeece819b978733879041d6
Crowdbotics <team@crowdbotics.com>
2022-04-11 10:08:46 +0000

    Update models

--- backend/home/migrations/0002_cycledata.py
+++ backend/home/migrations/0002_cycledata.py
@@ -8,15 +8,23 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('home', '0001_load_initial_data'),
+        ("home", "0001_load_initial_data"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='CycleData',
+            name="CycleData",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('cycleLength', models.BigIntegerField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("cycleLength", models.BigIntegerField()),
             ],
         ),
     ]
--- backend/home/models.py
+++ backend/home/models.py
@@ -5,3 +5,12 @@ from django.db import models
 class CycleData(models.Model):
     "Generated Model"
     cycleLength = models.BigIntegerField()
+    lastStartDay = models.DateTimeField(
+        auto_now=True,
+        null=True,
+        blank=True,
+    )
+    periodLength = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
